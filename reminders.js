99a4a46aee1af9158575a24aea44d400a4e00df1
Rod Paperfon <admin@paperfon.com>
2019-11-29 15:16:42 +0100

    Complete functionality plus saving the list to localStorage

--- reminders.js
+++ reminders.js
@@ -1,30 +1,16 @@
-var remindMeList = [];
+var remindersList = [];
+
+// Get the list if exists from localStorage
+function getRemindersList() {
+    const reminders = localStorage.getItem('remindersList');
+    if (reminders != null) {
+        remindersList = JSON.parse(reminders);
+    }
+    return remindersList;
+}
 
 // Adding an item to the list
 function addItemToList() {
-    //const content = document.getElementById("addItem").value;
-    //const item = {
-    //    content,
-    //    done: false,
-    //    id: Date.now(),
-    //};
-    //remindMeList.push(item);
-    //console.log(remindMeList);
-
-    //// Insert a li with the reminder item
-    //const listItems = document.getElementById("listItems");
-    //listItems.insertAdjacentHTML('beforeend', `
-    //    <li class="reminder-item" data-key="${item.id}">
-    //        <input id="${item.id}" type="checkbox" />
-    //        <label for="${item.id}" class="checked"></label>
-    //        <span>${item.content}</span>
-    //        <button class="delete-item">
-    //            <svg><use href="#delete-icon"></use></svg>
-    //        </button>
-    //    </li>
-    //        `);
-    //console.log(listItems);
-
 
     var content = document.getElementById("addItem").value;
     const item = {
@@ -32,36 +18,43 @@ function addItemToList() {
         done: false,
         id: Date.now(),
     };
-    var listItems = document.getElementById("listItems");
 
-    // Create a new HTML element and append it to the DOM
-    function appendOutput(item) {
-        //var newLi = document.createElement('li');
-        //newLi.setAttribute("id", value.replace(/\s+/g, ''));
-        //newLi.setAttribute("onclick", "removeItemFromList(this)");
-        //newLi.appendChild(document.createTextNode(value));
-        //listItems.append(newLi);
+    console.log(item);
 
-    listItems.insertAdjacentHTML('beforeend', `
-        <li class="reminder-item" data-key="${item.id}" id="${item.id}" onclick="removeItemFromList(this)">
-            <input id="${item.id}" type="checkbox" />
-            <label for="${item.id}" class="checked"></label>
-            <span>${item.content}</span>
-            <button class="delete-item">
-                <svg><use href="#delete-icon"></use></svg>
-            </button>
-        </li>
-            `);
-
-    }
-
+    const reminders = getRemindersList();
     if (content != "") {
-        remindMeList.push(item);
+        reminders.push(item);
         document.getElementById("addItemForm").reset();
-        console.log(remindMeList);
+        console.log(reminders);
         appendOutput(item);
     }
-    return false;
+    // Add to localStorage
+    localStorage.setItem('remindersList', JSON.stringify(reminders));
+}
+
+function appendOutput(item) {
+    //var newLi = document.createElement('li');
+    //newLi.setAttribute("id", value.replace(/\s+/g, ''));
+    //newLi.setAttribute("onclick", "removeItemFromList(this)");
+    //newLi.appendChild(document.createTextNode(value));
+    //listItems.append(newLi);
+
+    // Create a new HTML element and append it to the DOM
+
+    //<input id="${item.id}" type="checkbox" />
+    //<label for="${item.id}" class="checked"></label>
+
+    const listItems = document.getElementById("listItems");
+
+    listItems.insertAdjacentHTML('beforeend', `
+        <li class="reminder-item" id="${item.id}">
+            
+                <input type="checkbox" onclick="toggleItemDone(this)"/>
+                <span onclick="toggleItemDone(this)">${item.content}</span>
+                <button class="delete-item" onclick="removeItemFromList(${item.id})">Delete</button>
+            
+        </li>
+            `);
 }
 
 // Listener in the items list
@@ -69,22 +62,43 @@ function addItemToList() {
 //ulList.addEventListener("click", removeItemFromList, false);
 
 // Remove an item from the list
-function removeItemFromList(elem) {
-    var target = document.getElementById(elem.id);
-    console.log(target);
-    var index = remindMeList.indexOf(elem.id);
-    if (index > -1) {
-        remindMeList.splice(index, 1)
-    }
+function removeItemFromList(id) {
+    var target = document.getElementById(id);
+
+    let reminders = getRemindersList();
+    reminders = reminders.filter(item => item.id != id);
+    // Add to localStorage
+    localStorage.setItem('remindersList', JSON.stringify(reminders));
+
+    // Remove the element
     target.parentNode.removeChild(target);
-    console.log(remindMeList);
+
+    console.log(reminders);
 }
 
 // Delete the list and clear the content on the page
 function deleteList() {
-    remindMeList.length = 0;
+
+    let reminders = getRemindersList();
+    reminders.length = 0;
+    // Add to localStorage
+    localStorage.setItem('remindersList', JSON.stringify(reminders));
+
+    // Remove the elements
     while (listItems.firstChild != null) {
         listItems.removeChild(listItems.firstChild);
     }
-    console.log(remindMeList);
+    console.log(remindersList);
+}
+
+// Class toggler for the element
+function toggleItemDone(elem) {
+    elem.parentElement.classList.toggle("itemDone");
+}
+
+ //Show the existing items from localStorage
+function show() {
+    let reminders = getRemindersList();
+    reminders.forEach(item => appendOutput(item));
+    console.log(reminders);
 }
